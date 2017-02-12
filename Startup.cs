127e1720784243e25d6af1159c018a5c15e61f7b
zmosher <zachary.mosher@hotmail.com>
2017-02-12 10:59:19 -0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DiValVending.Startup))]
+namespace DiValVending
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
