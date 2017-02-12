01f5c4f318302b936d37c2b08d34e9cb9cfe3ce1
zmosher <zachary.mosher@hotmail.com>
2017-02-12 11:01:40 -0700

    Add files via upload

---
+++ Models/DiValVendingDb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DiValVending.Models
+{
+    public class DiValVendingDb : DbContext
+    {
+        public DiValVendingDb() : base("name=DefaultConnection")
+        { }
+
+        public DbSet<RegUser> RegUsers { get; set; }
+    }
+}
---
+++ Models/RegUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DiValVending.Models
+{
+    [Table("RegUser")]
+    public class RegUser
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int User_Index { get;}
+        
+    }
+}
